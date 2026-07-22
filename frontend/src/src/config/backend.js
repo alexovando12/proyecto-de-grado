19656cd82551b169d8ec81f700c6035c6c2fbd91
alexovando12 <alexanderob53@gmail.com>
2026-07-22 12:50:54 -0400

    fix: cambio finales

--- frontend/src/src/config/backend.js
+++
@@ -1,6 +0,0 @@
-const BACKEND_BASE_URL = (
-  import.meta.env.VITE_BACKEND_URL || "http://localhost:3000"
-).replace(/\/+$/, "");
-const BACKEND_API_URL = `${BACKEND_BASE_URL}/api`;
-
-export { BACKEND_BASE_URL, BACKEND_API_URL };
